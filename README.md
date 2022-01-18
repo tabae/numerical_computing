b23ae0c34592b800126688bad258b7fc6bb4b5ad
Ebata <dongurinao@gmail.com>
2022-01-18 23:42:27 +0900

    Add DFT

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 
 数値計算の勉強をかねて，C++で各種数値計算ライブラリを作成してみます．
 
-参考図書
+フーリエ変換
+-----------------------------
+- 離散フーリエ変換（愚直実装） [LINK](https://github.com/tabae/numerical_computing/fourier_transform/dft)
+
+参考
 -----------------------------
 - Fortran ハンドブック 田口俊弘 技術評論社
+- やる夫で学ぶディジタル信号処理 鏡 慎吾 [LINK](https://atcoder.jp/contests/agc041/tasks/agc041_b)
---
+++ fourier_transform/dft/dft.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <cmath>
+#include <complex>
+
+using namespace std;
+
+template<class T>
+vector<complex<T>> dft(const vector<complex<T>>& f_in) {
+    const T pi = acos(-1);
+    const int n_sample = f_in.size();
+    vector<complex<T>> f_out(n_sample);
+    for(int k = 0; k < n_sample; k++) {
+        for(int n = 0; n < n_sample; n++) {
+            complex<T> p(0, -2*pi/n_sample*k*n);
+            f_out[k] += f_in[n] * exp(p);
+        }
+    }
+    return f_out;
+}
+
+bool test() {
+    bool res = true;
+    const int n_sample = 200;
+    const double pi = acos(-1);
+    vector<complex<double>> f_in(n_sample);
+    for(int j = 0; j < n_sample; j++) {
+        for(int k = 0; k < n_sample; k++) {
+            const double p = 2*pi*k*j/n_sample; 
+            f_in[k] = complex<double>(cos(p), sin(p));
+        }
+        auto f_out = dft(f_in);
+        for(int n = 0; n < n_sample; n++) {
+            if(abs(f_out[n]) > 1e-9) {
+                if(abs(abs(f_out[n]) - n_sample) < 1e-9 && j == n) {
+                    cout << "[\e[0;32mSuccess\e[0m]\t"; 
+                } else {
+                    cout << "[\e[0;31mFailed\e[0m]\t"; 
+                    res = false;
+                }
+                cout << j << "," << n << ": " << f_out[n] << endl;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    assert(test());
+}
